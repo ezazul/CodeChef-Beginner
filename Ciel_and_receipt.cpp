fcdd2f259ebe3fe80913f09e7a45038e040535f8
Ezazul Haque Mulla <37624374+ezazul@users.noreply.github.com>
2018-04-10 02:39:35 +0530

    Add files via upload

--- Ciel_and_receipt.cpp
+++ Ciel_and_receipt.cpp
@@ -2,48 +2,40 @@
 #include<cmath>
 using namespace std;
 int main() {
-	//cin and cout is slower than scanf and printf, altough you can use these in c++
-	//however if you to this cin and cout will be fast
+	//cin and cout are slower than scanf and printf, altough you can use these in c++
+	//however if you set these cin and cout will be fast
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int t;
 	cin>>t;
 	while(t-->0){
-		int n, index, sum, count=1, flag = 0;
+		int n, index, sum, count=1;
 		cin>>n;
 		index = log(n)/log(2);
 		if(index >= 11){
 			index = 11;
 		}
 		sum = pow(2,index);
-		if(sum == n){
-			goto end;
-		}
-		while(index>=0){
+		if(sum != n){
 			while(sum != n){
-				int a = sum+pow(2,index); 
-				if(a > n){
-					index--;
-					break;
+				while(sum != n){
+					int a = sum+pow(2,index); 
+					if(a > n){
+						index--;
+						break;
+					}
+					else if(a == n){
+						count++;
+						sum = a;
+					}
+					else{
+						count++;
+						sum = a;
+					}
 				}
-				else if(a == n){
-					count++;
-					flag = 1;
-					sum = a;
-				}
-				else{
-					count++;
-					sum = a;
-				}
-			}
-			
-			if(flag == 1)
-			{
-				break;
 			}
 		}
-		end:
 		cout<<count<<endl;
 	}
 	return 0;
-}
+} 
